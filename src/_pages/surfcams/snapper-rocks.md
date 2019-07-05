880d883cc7fcb62e68d223d5248760903e91f8ab
liquidvisual <liquidvisual@gmail.com>
2019-07-06 03:00:26 +1000

    New Flow Player Template
    * No Vue, just JS
    * Some weirdness with poster, is flow using it?

---
+++ src/_pages/surfcams/snapper-rocks.md
@@ -0,0 +1,7 @@
+---
+layout: child_layout/surfcams_live_public
+title: Snapper Rocks
+permalink: /surfcams/snapper-rocks/
+user_type: public
+theme: theme-public
+---
--- src/assets/scripts/vue-components/Surfcam/SurfcamPlayerFlow.js
+++ src/assets/scripts/vue-components/Surfcam/SurfcamPlayerFlow.js
@@ -1,151 +1,175 @@
-/*
-    SURFCAM PLAYER FLOW
-    updated: 04.07.19
+// /*
+//     SURFCAM PLAYER FLOW
+//     updated: 04.07.19
 
-    USAGE:
-        <surfcam-player-flow
-            :muted="false"
-            :video-timeout="300">
-        </surfcam-player-flow>
-*/
-//-----------------------------------------------------------------
-// VIDEO PLAYER
-//
-// 1. component will either load video stream via selectedVideo (API)
-// 2. OR component will load stream through props set on the template
-//-----------------------------------------------------------------
+//     USAGE:
+//         <surfcam-player-flow
+//             :muted="false"
+//             :video-timeout="300">
+//         </surfcam-player-flow>
 
-Vue.component('surfcam-player-flow', {
-    template: `
-        <div class="video-player-flow collapse-sm-down">
+    // {% unless page.user_type == 'paid' %}
+    //     <link rel="stylesheet" href="//cdn.flowplayer.com/releases/native/stable/style/flowplayer.css">
+    //     <script src="//cdn.flowplayer.com/releases/native/stable/flowplayer.min.js"></script>
+    //     <!-- OPTIONAL PLUGINS -->
+    //     <script src="//cdn.flowplayer.com/releases/native/stable/plugins/hls.min.js"></script>
+    //     <script src="//imasdk.googleapis.com/js/sdkloader/ima3.js"></script>
+    //     <script src="//cdn.flowplayer.com/releases/native/stable/plugins/ads.min.js"></script>
+    //     <!-- <script src="//cdn.flowplayer.com/releases/native/stable/plugins/airplay.min.js"></script> -->
+    //     <!-- <script src="//cdn.flowplayer.com/releases/native/stable/plugins/chromecast.min.js"></script> -->
+    //     <!-- <script src="//cdn.flowplayer.com/releases/native/edge/plugins/keyboard.min.js"></script> -->
+    // {% endunless %}
+// */
+// //-----------------------------------------------------------------
+// // VIDEO PLAYER
+// //
+// // 1. component will either load video stream via selectedVideo (API)
+// // 2. OR component will load stream through props set on the template
+// //-----------------------------------------------------------------
 
-            <!-- ERRORS -->
-            <div
-                v-if="errors"
-                class="video-player-error"
-            >
-                <div>
-                    <i class="fa fa-warning"></i>
-                    <p>Sorry! We're experiencing technical difficulties with this surfcam.<br> We'll have it back up ASAP.</p>
-                </div>
-            </div>
+// Vue.component('surfcam-player-flow', {
+//     template: `
+//         <div class="video-player-flow collapse-sm-down">
 
-            <!-- WATERMARK -->
-            <div class="video-player-watermark"></div>
+//             <!-- ERRORS -->
+//             <div
+//                 v-if="errors"
+//                 class="video-player-error"
+//             >
+//                 <div>
+//                     <i class="fa fa-warning"></i>
+//                     <p>Sorry! We're experiencing technical difficulties with this surfcam.<br> We'll have it back up ASAP.</p>
+//                 </div>
+//             </div>
 
-            <!-- PLAYER -->
-            <div
-                ref="playerEl"
-                class="use-thin-controlbar use-controlbar-background"
-            >
-            </div>
+//             <!-- WATERMARK -->
+//             <div class="video-player-watermark"></div>
 
-        </div>
-    `,
-    props: {
-        muted: {
-            type: Boolean,
-            default: true
-        },
-        videoTimeout: {
-            type: Number,
-            default: 0 // 300 secs = 5 mins
-        }
-    },
-    data() {
-        return {
-            errors: false,
-            playerInstance: null,
-            poster: '/assets/img/layout/placeholder-video-1280x720.svg',
-            playerEl: null,
-            timeoutInstance: null
-        }
-    },
-    mounted() {
-        this.playerEl = this.$refs.playerEl;
+//             <!-- PLAYER -->
+//             <div
+//                 ref="playerEl"
+//                 class="use-thin-controlbar use-controlbar-background"
+//                 data-player-id="cad9d975-ccae-4757-88a3-a65ebb7419f8"
+//             >
+//             </div>
 
-        // INDEX WIDGET CALLS THESE
-        bus.$on('initPlayer', this.initPlayer);
-        bus.$on('loadVideo', this.loadVideo);
-    },
-    beforeDestroy() {
-        this.playerInstance.destroy();
-        this.playerInstance = null;
-    },
-    methods: {
-        //-----------------------------------------------------------------
-        // INIT PLAYER
-        //-----------------------------------------------------------------
+//         </div>
+//     `,
+//     props: {
+//         muted: {
+//             type: Boolean,
+//             default: true
+//         },
+//         videoTimeout: {
+//             type: Number,
+//             default: 0 // 300 secs = 5 mins
+//         }
+//     },
+//     data() {
+//         return {
+//             errors: false,
+//             playerInstance: null,
+//             poster: '/assets/img/layout/placeholder-video-1280x720.svg',
+//             playerEl: null,
+//             timeoutInstance: null
+//         }
+//     },
+//     mounted() {
+//         this.playerEl = this.$refs.playerEl;
 
-        initPlayer(command) {
+//         // INDEX WIDGET CALLS THESE
+//         bus.$on('initPlayer', this.initPlayer);
+//         bus.$on('loadVideo', this.loadVideo);
+//     },
+//     beforeDestroy() {
+//         this.playerInstance.destroy();
+//         this.playerInstance = null;
+//     },
+//     methods: {
+//         //-----------------------------------------------------------------
+//         // INIT PLAYER
+//         //-----------------------------------------------------------------
 
-            log(':: initPlayer() ::');
-            log('LIMITED DURATION: '+(command == 'limitDuration'));
+//         initPlayer(command) {
 
-            this.playerInstance = flowplayer(this.playerEl,
-            {
-                autoplay: true, // manually do it? - flowplayer won't show play icon when true
-                hls: { native: true }, // https://flowplayer.com/help/plugins/hls#configuration
-                loop: true,
-                muted: this.muted,
-                poster: this.poster,
-                src: null, // test: '//streamer.swellnet.com.au/surfcams/byron.stream/playlist.m3u8'
-                token: 'eyJraWQiOiJuN1JTclNTNVFBaTkiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJjIjoie1wiYWNsXCI6NixcImlkXCI6XCJuN1JTclNTNVFBaTlcIn0iLCJpc3MiOiJGbG93cGxheWVyIn0.M837aYfFdkiLKA1FSaPOL8GPip9MIlVOQkxkUDqXpfL15JKGpBEZB_kFNc3BDPeuXa8nJShZcGTXp2nhwOk7tA',
-                // ui: flowplayer.ui.NO_VOLUME_CONTROL | flowplayer.ui.NO_MUTE
-            });
+//             log(':: initPlayer() ::');
+//             log('LIMITED DURATION: '+(command == 'limitDuration'));
 
-            // setup timeout reset on play
-            this.playerInstance.on('play', (event) => {
-                if (command == 'limitDuration') this.startTimeout();
-                this.errors = false; // reset
-            });
-        },
-        //-----------------------------------------------------------------
-        // LOAD VIDEO
-        //-----------------------------------------------------------------
+//             this.playerInstance = flowplayer(this.playerEl,
+//             {
+//                 autoplay: true, // manually do it? - flowplayer won't show play icon when true
+//                 hls: { native: true }, // https://flowplayer.com/help/plugins/hls#configuration
+//                 loop: true,
+//                 muted: this.muted,
+//                 poster: this.poster,
+//                 src: null, // test: '//streamer.swellnet.com.au/surfcams/byron.stream/playlist.m3u8'
+//                 token: 'eyJraWQiOiJuN1JTclNTNVFBaTkiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJjIjoie1wiYWNsXCI6NixcImlkXCI6XCJuN1JTclNTNVFBaTlcIn0iLCJpc3MiOiJGbG93cGxheWVyIn0.M837aYfFdkiLKA1FSaPOL8GPip9MIlVOQkxkUDqXpfL15JKGpBEZB_kFNc3BDPeuXa8nJShZcGTXp2nhwOk7tA',
+//                 ui: flowplayer.ui.NO_VOLUME_CONTROL | flowplayer.ui.NO_MUTE,
+//                 ima: {
+//                     ads: [
+//                         {
+//                             time: 0,
+//                             adTag: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator='
+//                         }
+//                     ]
+//                 },
+//                 "plugins": [
+//                     "ads"
+//                 ]
+//             });
 
-        loadVideo(video_obj) {
-            this.playerInstance.poster = video_obj.image;
-            this.playerInstance.setSrc(video_obj.stream);
-            this.playerInstance.play();
+//             // setup timeout reset on play
+//             // this.playerInstance.on('play', (event) => {
+//             //     if (command == 'limitDuration') this.startTimeout();
+//             //     this.errors = false; // reset
+//             // });
+//         },
+//         //-----------------------------------------------------------------
+//         // LOAD VIDEO
+//         //-----------------------------------------------------------------
 
-            // Errors for non-hls only (iOS)
-            this.playerInstance.on('error', (event) => {
-                this.$emit('log-errors');
-                this.errors = true;
-                this.playerInstance.poster = this.poster; // iOS fix for weirdly large height image - undo the above
-            });
+//         loadVideo(video_obj) {
+//             this.playerInstance.poster = video_obj.image;
+//             this.playerInstance.setSrc(video_obj.stream);
+//             this.playerInstance.play();
 
-            this.startTimeout(); // start timer until pause
-        },
-        //-----------------------------------------------------------------
-        // UNLOAD VIDEO
-        //-----------------------------------------------------------------
+//             // Errors for non-hls only (iOS)
+//             this.playerInstance.on('error', (event) => {
+//                 this.$emit('log-errors');
+//                 this.errors = true;
+//                 this.playerInstance.poster = this.poster; // iOS fix for weirdly large height image - undo the above
+//             });
 
-        unloadVideo() {
-            this.resetTimeout();
-            this.playerInstance.setSrc('');
-        },
-        //-----------------------------------------------------------------
-        // START TIMEOUT
-        //-----------------------------------------------------------------
+//             // this.startTimeout(); // start timer until pause
+//         },
+//         //-----------------------------------------------------------------
+//         // UNLOAD VIDEO
+//         //-----------------------------------------------------------------
 
-        startTimeout() {
-            if (this.videoTimeout) {
-                this.resetTimeout();
-                this.timeoutInstance = setTimeout(() => {
-                    this.playerInstance.pause();
-                    this.resetTimeout();
-                }, Number(this.videoTimeout * 1000)); // 300 * 1000 = 300000ms = 5 minute timeout
-            }
-        },
-        //-----------------------------------------------------------------
-        // RESET TIMEOUT
-        //-----------------------------------------------------------------
+//         unloadVideo() {
+//             this.resetTimeout();
+//             this.playerInstance.setSrc('');
+//         },
+//         //-----------------------------------------------------------------
+//         // START TIMEOUT
+//         //-----------------------------------------------------------------
 
-        resetTimeout() {
-            clearTimeout(this.timeoutInstance);
-            this.timeoutInstance = null;
-        }
-    }
-});
+//         startTimeout() {
+//             if (this.videoTimeout) {
+//                 this.resetTimeout();
+//                 this.timeoutInstance = setTimeout(() => {
+//                     this.playerInstance.pause();
+//                     this.resetTimeout();
+//                 }, Number(this.videoTimeout * 1000)); // 300 * 1000 = 300000ms = 5 minute timeout
+//             }
+//         },
+//         //-----------------------------------------------------------------
+//         // RESET TIMEOUT
+//         //-----------------------------------------------------------------
+
+//         resetTimeout() {
+//             clearTimeout(this.timeoutInstance);
+//             this.timeoutInstance = null;
+//         }
+//     }
+// });
