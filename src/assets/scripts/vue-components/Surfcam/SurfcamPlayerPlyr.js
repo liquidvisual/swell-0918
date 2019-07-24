91514a0c4fc3adb4f7eb3f47f8cc858d5d4bfaf8
liquidvisual <liquidvisual@gmail.com>
2019-07-24 15:54:30 +1000

    Plyr error bug

--- src/assets/scripts/vue-components/Surfcam/SurfcamPlayerPlyr.js
+++ src/assets/scripts/vue-components/Surfcam/SurfcamPlayerPlyr.js
@@ -153,8 +153,11 @@ Vue.component('surfcam-player-plyr', {
                 this.hlsInstance.on(Hls.Events.ERROR, (event, data) => {
                     // if (data.fatal) this.$emit('log-errors');
                     if (data.fatal) {
-                        this.errors = true;
-                        this.poster = '/assets/img/layout/placeholder-video-1280x720.svg'; // iOS fix for weirdly large height image
+                        // there's a bug which causes error data not to write immediately, timeout solves this
+                        setTimeout(() => {
+                            this.errors = true;
+                            this.poster = '/assets/img/layout/placeholder-video-1280x720.svg'; // iOS fix for weirdly large height image
+                        }, 100);
                     }
                 });
             }
