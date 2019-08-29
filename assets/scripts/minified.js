c548a0c71a52783aca6243843f4f4381058f91c4
liquidvisual <liquidvisual@gmail.com>
2019-08-29 15:42:06 +1000

    Built liquidvisual from commit 0b23811 on branch master

--- assets/scripts/minified.js
+++ assets/scripts/minified.js
@@ -10787,16 +10787,12 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
       o = 0;function a(e) {
     var a = e.nextElementSibling.querySelector(".comment-content-wrapper .comment-header");o < t.length - 1 ? a.after(r) : (i.remove(), r.remove());var s = document.documentElement.clientWidth >= 1100 ? 80 : 0,
         l = document.documentElement.clientWidth >= 768 ? "smooth" : "auto";window.scrollTo({ top: e.getBoundingClientRect().top + window.pageYOffset - s, behavior: l }), n = t[++o].previousElementSibling;
-  }!function () {
-    if (t) {
-      n = t[o].previousElementSibling;var _e4 = document.querySelector(".pane-content .node-article .field-name-post-date"),
-          s = '\n\t\t\t\t<button class="btn btn-primary btn-small mb-3">\n\t\t\t\t\t<img class="mr-1" width="14" src="/assets/img/ui/comment-bubble.svg" alt="Comment Bubble" /> New Comments\n\t\t\t\t</button>\n\t\t\t',
-          l = '\n\t\t\t\t<button class="btn btn-primary btn-small btn-comment-next animated zoomIn faster">\n\t\t\t\t\tNext&nbsp;<i class="fa fa-arrow-down"></i>\n\t\t\t\t</button>\n\t\t\t';(i = new DOMParser().parseFromString(s, "text/html").body.firstChild).addEventListener("click", function () {
-        a(n);
-      }), (r = new DOMParser().parseFromString(l, "text/html").body.firstChild).addEventListener("click", function () {
-        a(n);
-      }), _e4.after(i);
-    }
+  }t && function () {
+    n = t[o].previousElementSibling;var e = document.querySelector(".main-body .field-name-body");console.log(e);(i = new DOMParser().parseFromString('\n\t\t\t<button class="btn btn-primary btn-small mb-3">\n\t\t\t\t<img class="mr-1" width="14" src="/assets/img/ui/comment-bubble.svg" alt="Comment Bubble" /> New Comments\n\t\t\t</button>\n\t\t', "text/html").body.firstChild).addEventListener("click", function () {
+      a(n);
+    }), (r = new DOMParser().parseFromString('\n\t\t\t<button class="btn btn-primary btn-small btn-comment-next animated zoomIn faster">\n\t\t\t\tNext&nbsp;<i class="fa fa-arrow-down"></i>\n\t\t\t</button>\n\t\t', "text/html").body.firstChild).addEventListener("click", function () {
+      a(n);
+    }), e && e.before(i);
   }();
 }();var resizeTimer,
     headroom = null,
