0b238111211db19933a62d1e9e2a6e822e688bf9
Jono Kingston <jono@swellnet.com>
2019-08-29 14:20:29 +1000

    Update the surfcam order

--- src/assets/scripts/vue-components/Multicam/MulticamSelect.js
+++ src/assets/scripts/vue-components/Multicam/MulticamSelect.js
@@ -78,8 +78,8 @@ Vue.component('multicam-select', {
                         { id: 786,    name: 'Shark Island',      path: 'shark-island.stream' },
                         { id: 886,    name: 'Cronulla Point',    path: 'cronulla-point.stream' },
                         { id: 891,    name: 'Cronulla Beaches',  path: 'cronulla-beaches.stream' },
-                        { id: 841,    name: 'Thirroul',          path: 'thirroul.stream' },
                         { id: 473140, name: 'Thirroul North', path: 'thirroul-north.stream' },
+                        { id: 841,    name: 'Thirroul',          path: 'thirroul.stream' },
                         { id: 791,    name: 'Wollongong',        path: 'wollongong.stream' },
                         { id: 472578, name: 'Kiama', path: 'kiama.stream' }
                     ]
