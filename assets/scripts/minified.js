feb779ffee1ff2aa7707dc114fa253e0521a159e
liquidvisual <liquidvisual@gmail.com>
2019-09-16 11:49:15 +1000

    Built liquidvisual from commit 1990739 on branch master

--- assets/scripts/minified.js
+++ assets/scripts/minified.js
@@ -10789,18 +10789,22 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
       n = "/sitemap.json?v=" + Date.now(),
       i = "https://swellnet-2018.yourwebvisual.com/sitemap.json?v=" + Date.now();var r = 0;!function n(o) {
     axios.get(o, "", { headers: { Accept: "*/*" } }).then(function (n) {
-      console.info("Success. Sitemap fetched.");var i = n.data;i && (document.querySelectorAll("[data-render-nav-children]").forEach(function (n) {
+      console.log("TEST: V15"), console.info("Success. Sitemap fetched.");var i = n.data;i && (document.querySelectorAll("[data-render-nav-children]").forEach(function (n) {
         var r = parseInt(n.dataset.renderNavChildren),
-            o = i[r].children;o && function n(i, r) {
-          r.forEach(function (r) {
-            var o = "" + ("/" == r.url && "/" == t ? "active" : "") + " " + ("" + (t.includes(r.url) && "/" != t ? "active" : "")) + " " + ("" + (r.children ? "has-dropdown" : "")) + " " + ("" + (r.classes ? r.classes : "")) + " " + ("" + (r.observed ? "is-observed" : "")) + " " + ("" + (r.premium ? "is-premium" : "")),
-                a = "<li class=\"" + o + "\">" + ("<a href=\"" + r.url + "\" data-alias=\"" + (r.alias ? r.alias : "") + "\">") + ("<span class=\"text\">" + r.name + "</span>") + ("" + (r.children ? '<span class="submenu-arrow"><i class="fa fa-angle-right"></i></span>' : "")) + ("" + (r.observed ? '<img class="obs-icon" title="Observed surf report" src="https://www.swellnet.com/assets/img/ui/obs-icon.svg" data-toggle="tooltip" alt="Observed">' : "")) + ("" + (r.premium ? '<img class="premium-icon" title="Premium surfcam for subscribers" src="https://www.swellnet.com/assets/img/ui/premium-icon.svg" data-toggle="tooltip" alt="Premium">' : "")) + "</a></li>",
-                s = e(a);if (i.append(s), r.children) {
-              var _t3 = "<ul class=\"dropdown\" data-parent=\"" + r.name + "\"></ul>",
-                  _i2 = e(_t3);s.append(_i2), n(_i2, r.children);
-            }
-          });
-        }(e(n), o);
+            o = i[r].children;if (o) {
+          var _i2 = window.location.origin,
+              _r2 = "https://shop.swellnet.com" === _i2 || "https://shopdev.swellnet.com" === _i2;var a = _r2 ? "https://www.swellnet.com" : "";!function n(i, r) {
+            var o = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "";
+            r.forEach(function (r) {
+              var a = "" + ("/" == r.url && "/" == t ? "active" : "") + " " + ("" + (t.includes(r.url) && "/" != t ? "active" : "")) + " " + ("" + (r.children ? "has-dropdown" : "")) + " " + ("" + (r.classes ? r.classes : "")) + " " + ("" + (r.observed ? "is-observed" : "")) + " " + ("" + (r.premium ? "is-premium" : "")),
+                  s = "<li class=\"" + a + "\">" + ("<a href=\"" + (o + r.url) + "\" data-alias=\"" + (r.alias ? r.alias : "") + "\">") + ("<span class=\"text\">" + r.name + "</span>") + ("" + (r.children ? '<span class="submenu-arrow"><i class="fa fa-angle-right"></i></span>' : "")) + ("" + (r.observed ? '<img class="obs-icon" title="Observed surf report" src="https://www.swellnet.com/assets/img/ui/obs-icon.svg" data-toggle="tooltip" alt="Observed">' : "")) + ("" + (r.premium ? '<img class="premium-icon" title="Premium surfcam for subscribers" src="https://www.swellnet.com/assets/img/ui/premium-icon.svg" data-toggle="tooltip" alt="Premium">' : "")) + "</a></li>",
+                  l = e(s);if (i.append(l), r.children) {
+                var _t3 = "<ul class=\"dropdown\" data-parent=\"" + r.name + "\"></ul>",
+                    _i3 = e(_t3);l.append(_i3), n(_i3, r.children, o);
+              }
+            });
+          }(e(n), o, a);
+        }
       }), megaMenu.init(), lvOffcanvas.init(), e('[data-toggle="tooltip"]').tooltip());
     }).catch(function (e) {
       if (console.warn("Error: failed to fetch sitemap. Retrying..."), ++r > 2) throw Error(response.statusText);n(i);
